7d4a003365528631131269aef01fea9379b003e0
vdelatorrelabs <vdelatorrelabs@533562ee-6239-11de-97f9-b393862d04d8>
2009-07-01 13:18:57 +0000

    setting the bases

---
+++ trunk/bwsp.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Vlabs Biological Web Services Proxy
+ *
+ * Copyright (c) 2009 Victor de la Torre (vdelatorre@cnio.es)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ * 
+ * http://code.google.com/p/bwsproxy/
+ *
+ */
+ 
+ include_once('class/class.proxy.php');
+ include_once('class/class.rest.php');
+ include_once('class/ez_sql.php');
+ include_once('class/xml2json.php');
+ 
+ //initialization
+ $debug 		= 1;
+ $currentTime 	= time();
+ $request		= '';
+ 
+ //proxy parameters
+ $service 		= $_REQUEST['bwsp_service'];					// service name
+ $format		= $_REQUEST['bwsp_output_format'];				// row|json
+ $soap	 		= $_REQUEST['bwsp_soap'];						// soap call
+ $restUrl 		= $_REQUEST['bwsp_rest'];						// rest call
+ $cache			= $_REQUEST['bwsp_cache'];						// 0|1|2|3|4|5
+ $callback		= $_REQUEST['bwsp_callback'];					// Callback function name
+ 
+ 
+ if (!isset($service) or  !isset($format) or !isset($cache) or (!isset($soap) and !isset($restUrl))){
+ 	//exit;
+ }
+
+ //calling a rest service (XML-RPC)
+ if ($restUrl){
+ 	 $rest 	= new Rest;
+ 	 if ($rest->callService($_REQUEST))
+ 		$rest->printResponse($format,$callback);
+ }
+
+?>
---
+++ trunk/class/class.proxy.php
@@ -0,0 +1,178 @@
+<?php
+/*
+ * Vlabs Biological Web Services Proxy
+ *
+ * Copyright (c) 2009 Victor de la Torre (vdelatorre@cnio.es)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ * 
+ * http://code.google.com/p/bwsproxy/
+ *
+ */
+
+ class Proxy {
+  
+ 	public $error 			= 0;
+ 	public $rowResponse		= ''; 
+ 	public $jsonResponse	= '';
+ 	
+ 	/**
+ 	 * serializeRequest function.
+ 	 * 
+ 	 * @access public
+ 	 * @param mixed $request
+ 	 * @return void
+ 	 */
+ 	function serializeRequest($request){
+
+		 $serialized = '';
+		 foreach ($request as $k=>$v){
+			 
+			 if (preg_match('/bwsp_/',$k) and !preg_match('/bwsp_callback/',$k) and !preg_match('/bwsp_service/',$k))
+			  	$serialized .= base64_encode($k.$v);
+ 	
+		 }
+		 return md5($serialized); 	
+  	}
+  	
+  	
+  	/**
+  	 * getMaxTime function.
+  	 * 
+  	 * @access public
+  	 * @param mixed $mode
+  	 * @return void
+  	 */
+  	function getMaxTime($currentTime,$mode){
+  		
+  		switch ($mode){
+			
+			case 0:
+				$maxTime = 0;											//no cache
+				break;													
+			case 1:
+				$maxTime = $currentTime - (60*60);						//1 hour
+				break;
+			
+			case 2:
+				$maxTime = $currentTime - (24*60*60);					//1 day
+				break;
+				
+			case 3:
+				$maxTime = $currentTime - (7*24*60*60);					//1 week
+				break;
+			
+			case 4:
+				$maxTime = $currentTime - (30*24*60*60);				//1 month
+				break;	
+				
+			case 5:
+				$maxTime = $currentTime - (365*24*60*60);				//1 year
+				break;	
+								
+		}
+		return $maxTime;
+  	}
+ 	
+
+ 	/**
+ 	 * saveCache function.
+ 	 * 
+ 	 * @access public
+ 	 * @param mixed $param
+ 	 * @param mixed $mode
+ 	 * @return void
+ 	 */
+ 	function saveCache($param){
+		
+		global $db;
+		
+		$currentTime 	= time();
+		$mode			= $param['bwsp_cache'];
+		$serialized 	= $this->serializeRequest($param);
+		
+		if ($mode){
+			$strSQL = "DELETE FROM cache WHERE request='".$serialized."'";
+			$db->query($strSQL);
+			
+			$strSQL = "INSERT INTO cache SET service='".$param['service']."', request='".$serialized."', row='".base64_encode($this->rowResponse)."', json='".base64_encode($this->jsonResponse)."', mode=".$mode.", lastUpdate=".$currentTime;
+			$db->query($strSQL);
+
+		}
+  	}
+  	
+  	/**
+  	 * getCache function.
+  	 * 
+  	 * @access public
+  	 * @param mixed $param
+  	 * @return void
+  	 */
+  	function getCache($param){
+  	
+  		global $db;
+		
+		$currentTime 	= time();
+		$serialized 	= $this->serializeRequest($param);
+
+		$strSQL = "SELECT * FROM cache WHERE request='".$serialized."'";
+		
+		$r = $db->get_row($strSQL);
+		
+		if ($r){
+			
+			if ($r->mode == 0)
+				return false;
+		
+			if ($r->lastUpdate >= $this->getMaxTime($currentTime,$r->mode)){
+				
+				$this->rowResponse = base64_decode($r->row);
+				$this->jsonResponse =  base64_decode($r->json);
+				return true;
+			}
+		}
+		return false;
+  	}
+  	
+  	
+  	
+  	/**
+  	 * callService function.
+  	 * 
+  	 * @access public
+  	 * @param mixed $param
+  	 * @return void
+  	 */
+  	function callService($param){
+  	
+  		if (!$this->getCache($param)){
+  			$this->getServiceResponse($param);
+  			$this->saveCache($param);
+  		}
+  		return true;
+  	
+  	}
+  	
+  	/**
+  	 * printResponse function.
+  	 * 
+  	 * @access public
+  	 * @param mixed $format
+  	 * @param mixed $callback
+  	 * @return void
+  	 */
+  	function printResponse($format,$callback){
+  		
+  		if ($format == 'row')
+  			echo $this->rowResponse; 
+  		else if ($format == 'json'){
+  			if ($callback)
+  				echo $callback.'('.$this->jsonResponse.');';
+  			else
+  				echo $this->jsonResponse;	
+  		}	
+  	}
+ 	
+ }
+
+?>
