8b9d90c9d4f96330efb20c7299e37ec232c407b9
vdelatorrelabs <vdelatorrelabs@533562ee-6239-11de-97f9-b393862d04d8>
2009-07-01 13:18:57 +0000

    setting the bases

---
+++ class/class.rest.php
@@ -0,0 +1,70 @@
+<?php 
+/*
+ * Vlabs Biological Web Services Proxy
+ *
+ * Copyright (c) 2009 Victor de la Torre (vdelatorre@cnio.es)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ * 
+ * http://code.google.com/p/bwsproxy/
+ *
+ */
+ 
+ class Rest extends Proxy{
+    
+
+    /**
+     * getServiceResponse function.
+     * 
+     * @access public
+     * @param mixed $param
+     * @return void
+     */
+    function getServiceResponse($param){
+	
+		$url = $param['bwsp_rest'];
+		
+		if (!$url)
+			return false;
+		
+		$u = parse_url($url);
+		
+		if (!$u)
+			return false;
+		
+		$buf ="POST ".$u['path']." HTTP/1.0\r\n";
+		$buf .="Host: ".$u['host']."\r\n";
+		$buf .="Content-type: application/x-www-form-urlencoded; charset=UTF-8\r\n";
+		$buf .="Content-length: ".strlen($u['query'])."\r\n";
+		$buf .="\r\n";
+		$buf .=$u['query'];
+
+		$fp = @fsockopen ($u['host'], 80);
+	
+		if (!$fp)
+			return false;
+
+		fputs($fp, $buf);
+		$buf ="";
+		while (!feof($fp))
+			$buf .=@fgets($fp,128);
+		fclose($fp);
+				
+		if (!$buf)
+			return false;
+
+		// split the result header from the content
+		$result = explode("\r\n\r\n", $buf, 2);
+		$header = isset($result[0]) ? $result[0] : '';
+		$content = isset($result[1]) ? $result[1] : '';
+			
+		if (!$content)
+			return false;
+
+		$this->rowResponse 	= $content;
+		$this->jsonResponse = xml2json::transformXmlStringToJson($this->rowResponse);
+		return true;
+	}
+ }
+ 
+?>
